ccf4b0ad75ef41cc1a3b3a830822601188b5fcd7
Paul A. Walrath <paul@walrath.ca>
2025-08-02 08:35:15 -0400

    Turbo rendering of wx

--- spec/requests/wx_spec.rb
+++ spec/requests/wx_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'webmock/rspec'
 
 RSpec.describe "WxController", type: :request do
   # NOTE: for simplicity, we'll use a single city's weather results
@@ -12,12 +13,7 @@ RSpec.describe "WxController", type: :request do
     JSON.parse(File.read(json_file))
   end
 
-  before do
-    stub_const('WEATHER_API_KEY', 'super-secret-key!!')
-    stub_request(:get, "#{WEATHER_API_BASE_URL}/current.json")
-      .with(query: hash_including(q: anything, key: WEATHER_API_KEY))
-      .to_return_json(status: 200, body: current_wx_observation)
-  end
+  before { stub_const('WEATHER_API_KEY', 'super-secret-key!!') }
 
   describe "GET /index" do
     it "returns the home page" do
@@ -28,12 +24,40 @@ RSpec.describe "WxController", type: :request do
   end
 
   describe "POST /query (via Turbo)" do
+    let(:turbo_headers) { { "Accept" => "text/vnd.turbo-stream.html" } }
+
     context "when getting current weather observations" do
+      before do
+        stub_request(:get, "#{WEATHER_API_BASE_URL}/current.json")
+          .with(query: hash_including(q: anything, key: WEATHER_API_KEY))
+          .to_return_json(status: 200, body: current_wx_observation)
+      end
+
       context "given a location and query-type" do
         it "displays the results below the query box" do
-          post wx_query_path, params: { type: :current, location: "Vancouver Canada"}
+          post wx_query_path,
+            params: { type: :current, location: "Vancouver Canada" },
+            headers: turbo_headers
           expect(response).to have_http_status(:success)
-          expect(response.body).to include('turbo-stream action="replace"')
+          expect(response.body).to include('turbo-stream action="update"')
+        end
+      end
+    end
+
+    context "when getting a forecast" do
+      before do
+        stub_request(:get, "#{WEATHER_API_BASE_URL}/forecast.json")
+          .with(query: hash_including(q: anything, key: WEATHER_API_KEY))
+          .to_return_json(status: 200, body: wx_forecast)
+      end
+
+      context "given a location and query-type" do
+        it "displays the results below the query box" do
+          post wx_query_path,
+            params: { type: :forecast, location: "Vancouver Canada" },
+            headers: turbo_headers
+          expect(response).to have_http_status(:success)
+          expect(response.body).to include('turbo-stream action="update"')
         end
       end
     end
@@ -46,5 +70,4 @@ RSpec.describe "WxController", type: :request do
       end
     end
   end
-
 end
