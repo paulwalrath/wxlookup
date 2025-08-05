34b550362db713c39ebbb29afe20133387844419
Paul A. Walrath <paul@walrath.ca>
2025-08-05 18:07:11 -0400

    Added encrypted API key

--- config/initializers/weather_api.rb
+++ config/initializers/weather_api.rb
@@ -1,3 +1,3 @@
 WEATHER_API_BASE_URL = ENV.fetch("WEATHER_API_BASE_URL", "https://api.weatherapi.com/v1").freeze
-WEATHER_API_KEY = ENV.fetch("WEATHER_API_KEY").freeze
+WEATHER_API_KEY = Rails.application.credentials.weather_api_key
 WEATHER_CACHE_TIMEOUT = 30.minutes
